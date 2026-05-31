c5299e92375fab1b082e2e7a018347ee4ddaf751
edcpilaresfspo-wq <edcpilaresfspo@gmail.com>
2026-05-31 14:58:58 -0600

    primera carga

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+const URL_API = "https://script.google.com/macros/s/AKfycbxCIk31wW7OvWpN0MvSqsPkl2Q5UUFRJ1MOA5EsZUXXUQhIvlnyTU2TPY48HAoB1BUsEQ/exec";
+
+// Guardar datos
+document.getElementById('miFormulario').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const mensaje = document.getElementById('folio').value;
+
+    await fetch(URL_API, {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ nombre, folio })
+    });
+
+    alert('¡Datos enviados con éxito!');
+    document.getElementById('miFormulario').reset();
+    cargarDatos();
+});
+
+// Leer y mostrar datos
+async function cargarDatos() {
+    const res = await fetch(URL_API);
+    const datos = await res.json();
+    const contenedor = document.getElementById('listaDatos');
+    contenedor.innerHTML = datos.map(fila => `<p><strong>${fila[0]}:</strong> ${fila[1]}</p>`).join('');
+}
+
+cargarDatos();
