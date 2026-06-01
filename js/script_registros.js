24915e2ac27c766f0604dd042ba1f68abea64fb9
edcpilaresfspo-wq <edcpilaresfspo@gmail.com>
2026-06-01 02:06:17 -0600

    Update script_registros.js
    Se quitaron unos alert que eran para pruebas.

--- js/script_registros.js
+++ js/script_registros.js
@@ -1,8 +1,6 @@
 const URL_API = "https://script.google.com/macros/s/AKfycbxCIk31wW7OvWpN0MvSqsPkl2Q5UUFRJ1MOA5EsZUXXUQhIvlnyTU2TPY48HAoB1BUsEQ/exec";
-alert("se ha ingresado al js registros"+URL_API);
 
 async function cargarDatos() {
-    alert("Ha ingresado a cargarDatos()");
     try {
         const res = await fetch(URL_API);
         const datos = await res.json();
